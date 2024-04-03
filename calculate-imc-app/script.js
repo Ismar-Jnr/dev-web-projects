d62fbec8d6bbc0af3258bfb0fab896fe2f297745
José Ismar <ismar.jr@icloud.com>
2024-04-03 20:39:27 -0300

    calculate-imc-app
    Aplicação para calcular IMC, imtegrando HTML, CSS e JavaScript.

---
+++ calculate-imc-app/script.js
@@ -0,0 +1,31 @@
+// Variáveis - variables
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
+
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modalMessage = document.querySelector('.modal .title span');
+const modalBtnClose = document.querySelector('.modal button.close');
+
+
+form.onsubmit = (event) => {
+    event.preventDefault(); // previne o recarregamento da pagina;
+
+    const weight = inputWeight.value;
+    const height = inputHeight.value;
+
+    const result = IMC(weight, height);
+    const message = `Seu IMC é de ${result}`;
+    
+    modalMessage.innerText = message;
+    modalWrapper.classList.add('open');
+}
+
+modalBtnClose.onclick = () => {
+    modalWrapper.classList.remove('open');
+}
+
+function IMC(weight, height){
+    return (weight/((height /100) ** 2)).toFixed(2);
+}
+
