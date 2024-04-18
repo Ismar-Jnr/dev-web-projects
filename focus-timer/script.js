c72e2cae9d5ddde12289e1c33d09c5e63d0ff7e7
José Ismar <ismar.jr@icloud.com>
2024-04-17 23:01:46 -0300

    focus-timer
    Dando inicio ao algoritmo de cronometro

--- focus-timer/script.js
+++ focus-timer/script.js
@@ -5,16 +5,50 @@
 const play = document.querySelector('.play');
 const pause = document.querySelector('.pause');
 const buttonStop = document.querySelector('.stop');
-const config = document.querySelector('.config'); 
+const configTempo = document.querySelector('.config-tempo'); 
 const soundOn = document.querySelector('.sound-on');
 const soundOff = document.querySelector('.sound-off');
+const minutos = document.querySelector('.minutos');
+const segundos = document.querySelector('.segundos');
+let minutosDefinidoPeloUser;
+let segundosDefinidoPeloUser;
 
+function diminuirContagem () {
+    setTimeout(()=> {
+        let segundosContagem = Number(segundos.textContent);
+        let minutosContagem = Number(minutos.textContent);
+
+        
+        if(segundosContagem <= 0){
+            segundosContagem =2;
+            
+            minutos.textContent = String(minutosContagem - 1).padStart(2, "0");
+        }
+        
+        segundos.textContent = String(segundosContagem - 1).padStart(2, "0");
+        
+        if(minutosContagem <= 0){
+
+            play.classList.remove('hide');
+            pause.classList.add('hide');
+            configTempo.classList.remove('hide');
+            buttonStop.classList.add('hide');
+
+            return
+        }
+        
+
+        diminuirContagem();
+    }, 1000)
+};
 
 play.addEventListener('click', () =>{
     play.classList.add('hide');
     pause.classList.remove('hide');
-    config.classList.add('hide');
+    configTempo.classList.add('hide');
     buttonStop.classList.remove('hide');
+
+    diminuirContagem();
 })
 
 pause.addEventListener('click', () =>{
@@ -27,7 +61,7 @@ pause.addEventListener('click', () =>{
 buttonStop.addEventListener('click', () => {
     play.classList.remove('hide');
     pause.classList.add('hide');
-    config.classList.remove('hide');
+    configTempo.classList.remove('hide');
     buttonStop.classList.add('hide');
 })
 
@@ -40,3 +74,12 @@ soundOff.addEventListener('click', () => {
     soundOn.classList.remove('hide');
     soundOff.classList.add('hide');
 })
+
+configTempo.addEventListener('click', () => {
+    minutosDefinidoPeloUser = prompt('Quantos minutos?');
+    minutos.textContent = minutosDefinidoPeloUser;
+    segundosDefinidoPeloUser = prompt('Quantos segundos?');
+    segundos.textContent = segundosDefinidoPeloUser;
+    
+})
+
