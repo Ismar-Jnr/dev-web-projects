3bf507609aa5049d2a4163deb12030a43b38b71b
José Ismar <ismar.jr@icloud.com>
2024-04-17 22:09:18 -0300

    focus-timer
    Criando as trocas de botões

--- focus-timer/script.js
+++ focus-timer/script.js
@@ -2,15 +2,41 @@
 // Document Object Model = Modelagem do documento em formato de objeto JavaScript
 
 //Atribuindo os elementos à suas devidas variaveis
-let play = document.querySelector('.play');
-let pause = document.querySelector('.pause');
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+const buttonStop = document.querySelector('.stop');
+const config = document.querySelector('.config'); 
+const soundOn = document.querySelector('.sound-on');
+const soundOff = document.querySelector('.sound-off');
+
 
 play.addEventListener('click', () =>{
     play.classList.add('hide');
     pause.classList.remove('hide');
+    config.classList.add('hide');
+    buttonStop.classList.remove('hide');
 })
 
-play.addEventListener('click', () =>{
+pause.addEventListener('click', () =>{
     play.classList.remove('hide');
     pause.classList.add('hide');
+    
+    
+})
+
+buttonStop.addEventListener('click', () => {
+    play.classList.remove('hide');
+    pause.classList.add('hide');
+    config.classList.remove('hide');
+    buttonStop.classList.add('hide');
+})
+
+soundOn.addEventListener('click', () => {
+    soundOn.classList.add('hide');
+    soundOff.classList.remove('hide');
+})
+
+soundOff.addEventListener('click', () => {
+    soundOn.classList.remove('hide');
+    soundOff.classList.add('hide');
 })
